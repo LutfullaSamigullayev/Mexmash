c350b802d52debfe554676edfefcb6a3e2596ae8
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-10-23 15:28:01 +0500

    token ni komentga oldim

--- src/components/AuthCheck.tsx
+++ src/components/AuthCheck.tsx
@@ -1,7 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export function AuthCheck() {
-    const access_token = localStorage.getItem("access_token");
+    // const access_token = localStorage.getItem("access_token");
+    // versalda production ga chiqardim portfolioga qo'shish uchun loginsiz kirish uchun token ni komentga oldim 
+    const access_token = true
     if (!access_token) {
     return <Navigate to="/login" />;
     }
